507f152cc84018db75511f98eef4deea343e3b44
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-01-28 15:35:30 +0330

    Front paging in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'welcome/index'
   devise_for :users , controllers: {registrations: "users/registrations"}
   resources :expenses
   resources :incomes
