8c15f5446c4c8e6b6a1334612798ef8f5872502b
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-01-29 17:58:28 +0330

    WIP: confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
-  devise_for :users , controllers: {registrations: "users/registrations", confirmations: "confirmations"}
+  devise_for :users , controllers: {registrations: "users/registrations", confirmations: "users/confirmations"}
   resources :expenses
   resources :incomes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
