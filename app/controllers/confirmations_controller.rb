00ba549c58ef0cd4dc7e57aa8ccca9d71b32cbbe
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-01-29 17:45:06 +0330

    Working on confirmations

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,2 +1,11 @@
 class ConfirmationsController < Devise::ConfirmationsController
+
+    def show
+        render 'confirmations/index'
+    end  
+
+    def after_confirmation_path_for(resource_name, resource)
+        sign_in(resource)
+        #render json: {"confirmation": "Sent!"}
+    end 
 end
